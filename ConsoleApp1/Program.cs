d765562f90918d6676d8bb30e8ebfa64c955b26e
visualstudio <visualstudio@122-PC36>
2019-01-30 10:47:17 -0500

    hvhgf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            class Node
+        {
+            class Village
+
+            {
+                // Node is an ADT
+                // what KInd of DATA do we need in a NODE?
+                public Village nextVillage;
+                public String VillageName;
+                public bool isAstrildeHere = false;
+
+            }
+
         }
+    }
+
+
+}
     }
 }
